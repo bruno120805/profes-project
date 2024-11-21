b36a25293477fc7380502170816c04fe24899e4c
Mateo <mateo.seg.gue@gmail.com>
2024-11-20 21:12:31 -0600

    Update landing page

--- frontend-next/src/app/page.tsx
+++ frontend-next/src/app/page.tsx
@@ -1,13 +1,11 @@
-import ContentWrapper from "@/components/ContentWrapper"
+import Hero from "@/components/HomePage/Hero"
 import MainNavigation from "@/components/MainNavigation"
 
 function HomePage() {
   return (
     <div>
-
-      <MainNavigation>
-        testing
-      </MainNavigation>
+      <MainNavigation> </MainNavigation>
+      <Hero />
     </div>
   )
 }
